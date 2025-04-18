b780199974dd9a14d3d4b608532a1a64c0b75c86
hariharan <hariharan.1985.s@gmail.com>
2025-04-18 22:39:50 +0530

    updated env config

--- src/app/predictor.service.ts
+++ src/app/predictor.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PredictorService {
-  private apiUrl = 'http://localhost:8080/predictions';
+  private apiUrl = environment.apiUrl;;
 
   constructor(private http: HttpClient) {}
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://phnumero.onrender.com/predictions'
+  };
